6bca284683e78ddec3064c70c00f99ed050f0238
pranavpal02 <pranav155.study@gmail.com>
2025-06-24 11:29:24 -0400

    Initial Home Page setup with DOC-12, DOC-13, DOC-14, DOC-15, DOC-39, DOC-40, DOC-42

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,19 +1,59 @@
-import React, { useState } from "react"
+import React, { useContext, useEffect, useState } from "react"
+import { AppContext } from "../context/AppContext"
+import axios from "axios"
+import { toast } from "react-toastify"
+import { useNavigate } from "react-router-dom"
 
 const Login = () => {
   const [state, setState] = useState("Sign Up")
 
+  const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-  const [name, setName] = useState("")
+
+  const navigate = useNavigate()
+  const { backendUrl, token, setToken } = useContext(AppContext)
 
   const onSubmitHandler = async (event) => {
     event.preventDefault()
+
+    if (state === "Sign Up") {
+      const { data } = await axios.post(backendUrl + "/api/user/register", {
+        name,
+        email,
+        password,
+      })
+
+      if (data.success) {
+        localStorage.setItem("token", data.token)
+        setToken(data.token)
+      } else {
+        toast.error(data.message)
+      }
+    } else {
+      const { data } = await axios.post(backendUrl + "/api/user/login", {
+        email,
+        password,
+      })
+
+      if (data.success) {
+        localStorage.setItem("token", data.token)
+        setToken(data.token)
+      } else {
+        toast.error(data.message)
+      }
+    }
   }
 
+  useEffect(() => {
+    if (token) {
+      navigate("/")
+    }
+  }, [token])
+
   return (
-    <form className="min-h-[80vh] flex items-center">
-      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg">
+    <form onSubmit={onSubmitHandler} className="min-h-[80vh] flex items-center">
+      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg">
         <p className="text-2xl font-semibold">
           {state === "Sign Up" ? "Create Account" : "Login"}
         </p>
@@ -21,45 +61,44 @@ const Login = () => {
           Please {state === "Sign Up" ? "sign up" : "log in"} to book
           appointment
         </p>
-        {state === "Sign Up" && (
-          <div className="w-full">
+        {state === "Sign Up" ? (
+          <div className="w-full ">
             <p>Full Name</p>
             <input
-              className="border border-zinc-300 rounded w-full p-2 mt-1"
-              type="text"
-              onChange={(e) => setName(e.target.name)}
+              onChange={(e) => setName(e.target.value)}
               value={name}
+              className="border border-[#DADADA] rounded w-full p-2 mt-1"
+              type="text"
               required
             />
           </div>
-        )}
-
-        <div className="w-full">
+        ) : null}
+        <div className="w-full ">
           <p>Email</p>
           <input
-            className="border border-zinc-300 rounded w-full p-2 mt-1"
-            type="email"
-            onChange={(e) => setEmail(e.target.name)}
+            onChange={(e) => setEmail(e.target.value)}
             value={email}
+            className="border border-[#DADADA] rounded w-full p-2 mt-1"
+            type="email"
             required
           />
         </div>
-        <div className="w-full">
+        <div className="w-full ">
           <p>Password</p>
           <input
-            className="border border-zinc-300 rounded w-full p-2 mt-1"
-            type="passwordrequired"
-            onChange={(e) => setPassword(e.target.name)}
+            onChange={(e) => setPassword(e.target.value)}
             value={password}
+            className="border border-[#DADADA] rounded w-full p-2 mt-1"
+            type="password"
             required
           />
         </div>
-        <button className="bg-primary text-white w-full py-2 rounded-md text-base">
-          {state === "Sign Up" ? "Create Account" : "Login"}
+        <button className="bg-primary text-white w-full py-2 my-2 rounded-md text-base">
+          {state === "Sign Up" ? "Create account" : "Login"}
         </button>
         {state === "Sign Up" ? (
           <p>
-            Already have an account?
+            Already have an account?{" "}
             <span
               onClick={() => setState("Login")}
               className="text-primary underline cursor-pointer"
@@ -69,12 +108,12 @@ const Login = () => {
           </p>
         ) : (
           <p>
-            Create a new account?
+            Create an new account?{" "}
             <span
               onClick={() => setState("Sign Up")}
               className="text-primary underline cursor-pointer"
             >
-              click here
+              Click here
             </span>
           </p>
         )}
---
+++ frontend/src/pages/Verify.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Verify = () => {
+  return <div></div>
+}
+
+export default Verify
