fcf8de24e2499ce93a0f41720069da6972ad3e7f
pranavpal02 <pranav155.study@gmail.com>
2025-07-15 01:51:12 -0400

    Added the admin portal dashboard to add doctors, getting doctors on frontend through api.

---
+++ backend/controllers/doctorController.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+import doctorModel from "../models/doctorModel.js"
+
+// API to change doctor availablity for Admin and Doctor Panel
+const changeAvailablity = async (req, res) => {
+  try {
+    const { docId } = req.body
+
+    const docData = await doctorModel.findById(docId)
+    await doctorModel.findByIdAndUpdate(docId, {
+      available: !docData.available,
+    })
+    res.json({ success: true, message: "Availablity Changed" })
+  } catch (error) {
+    console.log(error)
+    res.json({ success: false, message: error.message })
+  }
+}
+
+// API to get all doctors list for Frontend
+const doctorList = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({}).select(["-password", "-email"])
+    res.json({ success: true, doctors })
+  } catch (error) {
+    console.log(error)
+    res.json({ success: false, message: error.message })
+  }
+}
+
+export { changeAvailablity, doctorList }
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,13 +1,38 @@
 // AppContext.jsx
-import { createContext } from "react"
-import { doctors } from "../assets/assets"
+import { createContext, useEffect, useState } from "react"
+import { toast } from "react-toastify"
+import axios from "axios"
 
 export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
+  const currencySymbol = "$"
+  const backendUrl = import.meta.env.VITE_BACKEND_URL
+  const [doctors, setDoctors] = useState([])
+
   const value = {
     doctors,
+    currencySymbol,
   }
+
+  // Getting Doctors using API
+  const getDoctosData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/doctor/list")
+      if (data.success) {
+        setDoctors(data.doctors)
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+  }
+
+  useEffect(() => {
+    getDoctosData()
+  }, [])
 
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
